0751e5d13dee2e8475d625547ff5dcf4803c1c13
Naveena Ponna <Naveena@welkin-digital.com>
2024-03-21 11:39:29 +0530

    Static api using http fs packages

---
+++ OneDrive/Desktop/nodejs-concepts/inbuiltpackages/staticapi.js
@@ -0,0 +1,13 @@
+let fs = require('fs');
+let http = require('http');
+
+let server = http.createServer((req,res)=>{
+      //read file with fs package
+    fs.readFile('city.json','utf-8',function(err,data){
+        if(err) throw err;
+          // return data of file
+        res.write(data);
+        res.end();
+    })
+}) 
+server.listen(6700) 
